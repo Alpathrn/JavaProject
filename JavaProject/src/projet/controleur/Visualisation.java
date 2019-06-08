7dbad80bb3d3fd1c34d6d07462d479542956eb10
Henrydlf <36404566+Henrydlf@users.noreply.github.com>
2019-06-08 11:31:48 +0200

    visu

--- JavaProject/src/projet/controleur/Visualisation.java
+++ JavaProject/src/projet/controleur/Visualisation.java
@@ -9,42 +9,116 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.table.AbstractTableModel;
 
 /**
  *
  * @author hdela
  */
-public class Visualisation {
-    private String table;
+public class Visualisation extends AbstractTableModel{
+    private Connexion m_conn;
+    private String m_choix_cat;
     
     /**
      * constructeur de la JTable
-     * @param _table, nom de la table a aller recuperer dans la BDD
-     * 
+     * @param choix_cat
+     * @param conn
      */
-    public Visualisation(String _table)
+    public Visualisation(String choix_cat, Connexion conn)
     {
-        this.table=_table;
+        this.m_conn= conn;
+        this.m_choix_cat = choix_cat;
+        
     }
     
     public Visualisation()
     {
-        this.table= null;
+        this.m_choix_cat= null;
     }
     
     /**
-     * 
      * @return le nombre de ligne: toutes dans chaque table grace a SELECT * FROM table
      */
-    public int getRowCount(Connexion conn, String choix_cat) {
+    @Override
+    public int getRowCount() {
         int count=0;
         try {
-            conn.setRset(conn.getStmt().executeQuery("SELECT * FROM "+choix_cat));
-            conn.getRset().last();
-            count = conn.getRset().getRow();
+            m_conn.setRset(m_conn.getStmt().executeQuery("SELECT * FROM "+m_choix_cat));
+            m_conn.getRset().last();
+            count = m_conn.getRset().getRow();
         } catch (SQLException ex) {
             Logger.getLogger(Visualisation.class.getName()).log(Level.SEVERE, null, ex);
         }
         return count;
+    }
+
+    @Override
+    public int getColumnCount() {
+        int count=0;
+        switch(m_choix_cat){
+            case "annéescolaire":
+                count = 1;
+                break;
+            case "bulletin":
+                count = 4;
+                break;
+            case "classe":
+                count = 5;
+                break;
+            case "detail bulletin":
+                count = 4;
+                break;
+            case "discipline":
+                count = 2;
+                break;
+            case "ecole":
+                count = 2;
+                break;
+            case "enseignement":
+                count = 4;
+                break;
+            case "evaluation":
+                count = 4;
+                break; 
+            case "inscription":
+                count = 3;
+                break;
+            case "niveau":
+                count = 2;
+                break;    
+            case "personne":
+                count = 4;
+                break;
+            case "trimestre":
+                count = 5;
+                break;    
+        }
+        return count;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        try {
+                m_conn.getRset().absolute(rowIndex + 1);
+                switch (columnIndex) {
+                    case 0: 
+                        return m_conn.getRset().getString(1);
+                    case 1:
+                        return m_conn.getRset().getString(2);
+                    case 2:
+                        return m_conn.getRset().getString(3);
+                    case 3:
+                        return m_conn.getRset().getString(4);
+                    case 4:
+                        return m_conn.getRset().getString(5);
+                    case 5:
+                        return m_conn.getRset().getString(6);
+                    default:
+                        return null;
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
     }
 }
--- JavaProject/src/projet/view/FenetreVisualisation.java
+++ JavaProject/src/projet/view/FenetreVisualisation.java
@@ -6,10 +6,13 @@
 package projet.view;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.JTableHeader;
 import projet.controleur.Connexion;
 import projet.controleur.Visualisation;
 
@@ -18,24 +21,101 @@ import projet.controleur.Visualisation;
  * @author hdela
  */
 public class FenetreVisualisation extends JFrame{
+    
+    JTableHeader th_visu;
         
     public FenetreVisualisation(Connexion conn){
         super("Visualisation");
         setTitle("Visualisation de la BDD"); 
 	setSize(750, 625);
         
-        JTable tb_visu = new JTable();                    
-        tb_visu.setBounds(20, 100, 700, 550);
+        JTable tb_visu = new JTable();
+        tb_visu.setBounds(50, 100, 700, 550);
         JScrollPane scr_visu = new JScrollPane(tb_visu);
-        scr_visu.setBounds(20, 100, 700, 550);
+        scr_visu.setBounds(20, 100, 700, 550);  
         
-        String[] options_table = { "Sélectionner une table", "annee scolaire", "bulletin", "classe", "detail bulletin", "discipline", "ecole", "enseignement", "evaluation", "inscription", "niveau", "personne", "trimestre"};
+        String[] options_table = { "Sélectionner une table", "annéescolaire", "bulletin", "classe", "detailbulletin", "discipline", "ecole", "enseignement", "evaluation", "inscription", "niveau", "personne", "trimestre"};
         JComboBox cb_visu = new JComboBox(options_table); 
         cb_visu.setBounds(10, 10, 10, 10);
         
-        Visualisation visu = new Visualisation();
+        cb_visu.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                tb_visu.setModel(new Visualisation((String) cb_visu.getSelectedItem(), conn));
+                th_visu = tb_visu.getTableHeader();
+                switch((String) cb_visu.getSelectedItem())
+                {
+                    case "annéescolaire":
+                        th_visu.getColumnModel().getColumn(0).setHeaderValue("Id");
+                        break;
+                    case "bulletin":
+                        th_visu.getColumnModel().getColumn(0).setHeaderValue("Id");
+                        th_visu.getColumnModel().getColumn(1).setHeaderValue("Trimestre");
+                        th_visu.getColumnModel().getColumn(2).setHeaderValue("Inscription");
+                        th_visu.getColumnModel().getColumn(3).setHeaderValue("Appreciation");
+                        break;
+                    case "classe":
+                        th_visu.getColumnModel().getColumn(0).setHeaderValue("Id");
+                        th_visu.getColumnModel().getColumn(1).setHeaderValue("Nom");
+                        th_visu.getColumnModel().getColumn(2).setHeaderValue("Ecole");
+                        th_visu.getColumnModel().getColumn(3).setHeaderValue("Niveau");
+                        th_visu.getColumnModel().getColumn(4).setHeaderValue("Année Scolaire");
+                        break;
+                    case "detail bulletin":
+                        th_visu.getColumnModel().getColumn(0).setHeaderValue("Id");
+                        th_visu.getColumnModel().getColumn(1).setHeaderValue("Bulletin");
+                        th_visu.getColumnModel().getColumn(2).setHeaderValue("Enseignement");
+                        th_visu.getColumnModel().getColumn(3).setHeaderValue("Appreciation");
+                        break;
+                    case "discipline":
+                        th_visu.getColumnModel().getColumn(0).setHeaderValue("Id");
+                        th_visu.getColumnModel().getColumn(1).setHeaderValue("Nom");
+                        break;
+                    case "ecole":
+                        th_visu.getColumnModel().getColumn(0).setHeaderValue("Id");
+                        th_visu.getColumnModel().getColumn(1).setHeaderValue("Nom");
+                        break;
+                    case "enseignement":
+                        th_visu.getColumnModel().getColumn(0).setHeaderValue("Id");
+                        th_visu.getColumnModel().getColumn(1).setHeaderValue("Classe");
+                        th_visu.getColumnModel().getColumn(2).setHeaderValue("Discipline");
+                        th_visu.getColumnModel().getColumn(3).setHeaderValue("Personne");
+                        break;
+                    case "evaluation":
+                        th_visu.getColumnModel().getColumn(0).setHeaderValue("Id");
+                        th_visu.getColumnModel().getColumn(1).setHeaderValue("Detail Bulletin");
+                        th_visu.getColumnModel().getColumn(2).setHeaderValue("Note");
+                        th_visu.getColumnModel().getColumn(3).setHeaderValue("Appreciation");
+                        break; 
+                    case "inscription":
+                        th_visu.getColumnModel().getColumn(0).setHeaderValue("Id");
+                        th_visu.getColumnModel().getColumn(1).setHeaderValue("Classe");
+                        th_visu.getColumnModel().getColumn(2).setHeaderValue("Personne");
+                        break;
+                    case "niveau":
+                        th_visu.getColumnModel().getColumn(0).setHeaderValue("Id");
+                        th_visu.getColumnModel().getColumn(1).setHeaderValue("Nom");
+                        break;    
+                    case "personne":
+                        th_visu.getColumnModel().getColumn(0).setHeaderValue("Id");
+                        th_visu.getColumnModel().getColumn(1).setHeaderValue("Nom");
+                        th_visu.getColumnModel().getColumn(2).setHeaderValue("Prenom");
+                        th_visu.getColumnModel().getColumn(3).setHeaderValue("Type");
+                        break;
+                    case "trimestre":
+                        th_visu.getColumnModel().getColumn(0).setHeaderValue("Id");
+                        th_visu.getColumnModel().getColumn(1).setHeaderValue("Numero");
+                        th_visu.getColumnModel().getColumn(2).setHeaderValue("Debut");
+                        th_visu.getColumnModel().getColumn(3).setHeaderValue("Fin");
+                        th_visu.getColumnModel().getColumn(4).setHeaderValue("Année scolaire");
+                        break;    
+                }
+            }
+        });
         
-        this.getContentPane().add(tb_visu,BorderLayout.CENTER);
+        //this.getContentPane().add(tb_visu,BorderLayout.CENTER);
         this.getContentPane().add(scr_visu);
         this.getContentPane().add(cb_visu, BorderLayout.NORTH);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
